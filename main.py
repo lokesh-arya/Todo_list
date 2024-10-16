cf9b8b0af1b7e7c632f9da8e70388c5185f5f5d2
lokesh-arya <lokesh2arya@gmail.com>
2024-10-16 23:30:52 +0530

    Update main.py
    template

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello World")
+class Task:
+    def __init__(self, title, description, category):
+        self.title = title
+        self.description = description
+        self.category = category
+        self.completed = False
+    def mark_completed(self):
+        self.completed = True
+
+#Functions
+def add_task():
+    pass
+
+def mark_task():
+    pass
+  
+#Menu
+
+while True:
+    try:
+        pass
+    except:
+        pass
