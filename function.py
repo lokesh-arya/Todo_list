43b6a9f80551a81a1b8b60872f95f512bc5ca173
lokesh-arya <lokesh2arya@gmail.com>
2024-10-16 23:36:39 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,15 @@
+class Task:
+    def __init__(self, title, description, category):
+        self.title = title
+        self.description = description
+        self.category = category
+        self.completed = False
+    def mark_completed(self):
+        self.completed = True
+
+#Functions
+def add_task():
+    pass
+
+def mark_task():
+    pass
