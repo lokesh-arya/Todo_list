3be1d415c436b48ced4cb05dd1ac394ce3cbe1bc
lokesh-arya <lokesh2arya@gmail.com>
2024-10-18 16:33:23 +0530

    Update application.py

--- application.py
+++ application.py
@@ -7,38 +7,55 @@ root = tk.Tk()
 root.title('Personal TO-DO List')
 root.geometry('500x600')
 
-def destory_all_task():
-    for child in work_frame.winfo_children():
-        child.destroy()
-    for child in personal_frame.winfo_children():
-        child.destroy()
-    for child in urgent_frame.winfo_children():
-        child.destroy()
-
-def mark_complete_task(index):
-    app.mark_completed(index)
-    destory_all_task()
-    display_task(task_list)
-    print("done")
-
 def cancel_function():
     for child in edit_window.winfo_children():
         child.destroy()
+    for child in add_task_window.winfo_children():
+        child.destroy()
     edit_window.pack_forget()
+    add_task_window.pack_forget()
     window.pack()
 
-def save_function(index, title, description):
-    task_list[index].title = str(title.get())
-    task_list[index].description = str(description.get())
-    app.save_tasks()
+def save_function(index, title, description, category):
+    if index:
+        task_list[index].title = str(title.get())
+        task_list[index].description = str(description.get())
+        task_list[index].category = str(category.get())
+        app.save_tasks()
+    else:
+        app.add_task(str(title.get()), str(description.get()), str(category.get()), False)
 
     for child in edit_window.winfo_children():
         child.destroy()
+    for child in add_task_window.winfo_children():
+        child.destroy()
     edit_window.pack_forget()
+    add_task_window.pack_forget()
     window.pack()
     destory_all_task()
     display_task(task_list)
     print("done")
+
+def add_task_function():
+    window.pack_forget()
+    add_task_window.pack()
+
+    title_var = tk.StringVar()
+    description_var = tk.StringVar()
+    category_var = tk.StringVar()
+
+    new_title = tk.Entry(add_task_window, textvariable=title_var)
+    new_description = tk.Entry(add_task_window, textvariable=description_var)
+    new_category = tk.Entry(add_task_window, textvariable=category_var)
+
+    new_title.pack()
+    new_description.pack()
+    new_category.pack()
+
+    cancel_button = tk.Button(add_task_window,text='Cancel', command=lambda: cancel_function())
+    save_button = tk.Button(add_task_window,text='Save', command=lambda: save_function(False, title_var, description_var, category_var))
+    cancel_button.pack(side=tk.LEFT)
+    save_button.pack(side=tk.LEFT)
 
 def edit_task(index):
     window.pack_forget()
@@ -48,17 +65,27 @@ def edit_task(index):
     title_var.set(task_list[index].title)
     description_var = tk.StringVar()
     description_var.set(task_list[index].description)
+    category_var = tk.StringVar()
+    category_var.set(task_list[index].category)
 
     edit_title = tk.Entry(edit_window, textvariable=title_var)
     edit_description = tk.Entry(edit_window, textvariable=description_var)
+    edit_category = tk.Entry(edit_window, textvariable=category_var)
 
     edit_title.pack()
     edit_description.pack()
+    edit_category.pack()
 
     cancel_button = tk.Button(edit_window,text='Cancel', command=lambda: cancel_function())
-    save_button = tk.Button(edit_window,text='Save', command=lambda: save_function(index, title_var, description_var))
+    save_button = tk.Button(edit_window,text='Save', command=lambda: save_function(index, title_var, description_var, category_var))
     cancel_button.pack(side=tk.LEFT)
     save_button.pack(side=tk.LEFT)
+
+def mark_complete_task(index):
+    app.mark_completed(index)
+    destory_all_task()
+    display_task(task_list)
+    print("done")
 
 def delete_task(index):
     app.delete_task(index)
@@ -93,9 +120,6 @@ def task_frame(frame, task, index):
     delete_button = tk.Button(footer, text='Delete', command=lambda: delete_task(index), bg='lightblue')
     delete_button.pack(padx=10, pady=10, side=tk.LEFT)
 
-
-    
-
 def display_task(task_list):
     for i in range(len(task_list)):
         if task_list[i].category == 'work':
@@ -107,7 +131,13 @@ def display_task(task_list):
         if task_list[i].category == 'urgent':
             task_frame(work_frame, task_list[i], index=i)
 
-
+def destory_all_task():
+    for child in work_frame.winfo_children():
+        child.destroy()
+    for child in personal_frame.winfo_children():
+        child.destroy()
+    for child in urgent_frame.winfo_children():
+        child.destroy()
 
 def show_frame(frame):
     # Hide all frames
@@ -117,26 +147,28 @@ def show_frame(frame):
     frame.pack(fill='both', expand=True)
 
 # Create the main window
-# category button
 window = tk.Frame(root)
 window.pack(fill='both',expand=True)
 edit_window = tk.Frame(root)
+add_task_window = tk.Frame(root)
 menu = tk.Frame(window, bg='lightblue')
-menu.pack()
+menu.pack(fill='x')
 
+# category button
 btn_work = tk.Button(menu, text="Work", command=lambda: show_frame(work_frame))
 btn_personal = tk.Button(menu, text="Personal", command=lambda: show_frame(personal_frame))
 btn_urgent = tk.Button(menu, text="Urgent", command=lambda: show_frame(urgent_frame))
+btn_add_task = tk.Button(menu, text='[+] Add Task', command=lambda: add_task_function())
 
 btn_work.pack(side=tk.LEFT, padx=5, pady=5)
 btn_personal.pack(side=tk.LEFT, padx=5, pady=5)
 btn_urgent.pack(side=tk.LEFT, padx=5, pady=5)
+btn_add_task.pack(side=tk.RIGHT, padx=5, pady=5)
 
 # Create frames
 work_frame = tk.Frame(window, bg='red')
 personal_frame = tk.Frame(window, bg='green')
 urgent_frame = tk.Frame(window, bg='blue')
-
 
 # Store frames in a list
 frames = [work_frame, personal_frame, urgent_frame]
@@ -147,10 +179,5 @@ display_task(task_list)
 
 # Start with the first frame
 show_frame(work_frame)
-
-# Create buttons to switch frames
-
-print(app.tasks)
-print(type(app.tasks[0]))
 
 root.mainloop()
